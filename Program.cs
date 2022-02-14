767906b2a479679f3894f112879a5fef4905dcc1
indhumathi <Indhumathi.Ramesh@disys.com>
2022-02-14 07:30:43 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+public class Multithreading
+{
+    public static void Thread1()
+    {
+        for (int value1 = 0; value1 <= 4; value1++)
+        {
+            Console.WriteLine("Thread one executing");
+            if (value1 == 3)
+            {
+                Thread.Sleep(6000);
+            }
+        }
+    }
+    public static void Thread2()
+    {
+        for (int value2 = 0; value2 <= 4; value2++)
+        {
+            Console.WriteLine("Thread two executing");
+        }
+    }
+    static public void Main()
+    {
+        Thread thread1 = new Thread(Thread1);
+        Thread thread2 = new Thread(Thread2);
+        thread1.Start();
+        thread2.Start();
+    }
+}
